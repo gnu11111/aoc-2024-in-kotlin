240e37d16cdb7fcd75a79fa741ce929b18459f60
gnu11111 <gnu@gnu.at>
2024-12-03 13:55:20 +0100

    polishing

--- src/main/kotlin/at/gnu/adventofcode/year2024/Day03.kt
+++ src/main/kotlin/at/gnu/adventofcode/year2024/Day03.kt
@@ -5,20 +5,21 @@ class Day03(private val memory: String) {
     private val commands = """(mul)\((\d{1,3}),(\d{1,3})\)|(do)\(\)|(don't)\(\)""".toRegex()
 
     fun part1(): Long =
-        commands.findAll(memory).fold(0L) { acc, parameters ->
-            val (multiplication, number1, number2) = parameters.destructured
+        commands.findAll(memory).fold(0L) { acc, command ->
+            val (multiplication, number1, number2) = command.destructured
             if (multiplication.isNotBlank()) (acc + (number1.toLong() * number2.toLong())) else acc
         }
 
     fun part2(): Long =
-        commands.findAll(memory).fold(Pair(0L, true)) { acc, match ->
+        commands.findAll(memory).fold(Pair(0L, true)) { acc, command ->
             val result = acc.first
             val enabled = acc.second
-            val (_, number1, number2, switchOn, switchOff) = match.destructured
+            val (_, number1, number2, switchOn, switchOff) = command.destructured
             when {
                 switchOn.isNotBlank() -> result to true
                 switchOff.isNotBlank() -> result to false
-                else -> (result + if (enabled) (number1.toLong() * number2.toLong()) else 0L) to enabled
+                enabled -> (result + (number1.toLong() * number2.toLong())) to true
+                else -> result to enabled
             }
         }.first
 
