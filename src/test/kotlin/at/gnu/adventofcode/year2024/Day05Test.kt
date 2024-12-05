84cf983cb236c1702de19dff7f08f1eddcb22029
gnu11111 <gnu@gnu.at>
2024-12-05 23:57:30 +0100

    polishing

--- src/test/kotlin/at/gnu/adventofcode/year2024/Day05Test.kt
+++ src/test/kotlin/at/gnu/adventofcode/year2024/Day05Test.kt
@@ -9,7 +9,7 @@ class Day05Test {
 
     private val rules = listOf("47|53", "97|13", "97|61", "97|47", "75|29", "61|13", "75|53", "29|13", "97|29",
         "53|29", "61|53", "97|53", "61|29", "47|13", "75|47", "97|75", "47|61", "75|61", "47|29", "75|13", "53|13")
-    private val pages = listOf("75,47,61,53,29", "97,61,53,29,13", "75,29,13", "75,97,47,61,53", "61,13,29",
+    private val updates = listOf("75,47,61,53,29", "97,61,53,29,13", "75,29,13", "75,97,47,61,53", "61,13,29",
         "97,13,75,29,47")
 
     private val test = mapOf(
@@ -19,10 +19,10 @@ class Day05Test {
 
     @Test @ExperimentalTime
     fun testMySolution() {
-        val day05 = Day05(rules, pages)
+        val day05 = Day05(rules, updates)
         for (function in test.keys) {
             val (result, time) = measureTimedValue { function(day05) }
-            println("Day05::${function.name}: ${rules.size} rules, ${pages.size} pages -> $result [$time]")
+            println("Day05::${function.name}: ${rules.size} rules, ${updates.size} updates -> $result [$time]")
             assertEquals(test[function], result)
         }
     }
