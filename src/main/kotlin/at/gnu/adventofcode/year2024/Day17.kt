2fbb5ef39267ad5a67773fd920b62469a8053a7c
gnu11111 <gnu@gnu.at>
2024-12-18 17:43:52 +0100

    polishing

--- src/main/kotlin/at/gnu/adventofcode/year2024/Day17.kt
+++ src/main/kotlin/at/gnu/adventofcode/year2024/Day17.kt
@@ -2,17 +2,16 @@ package at.gnu.adventofcode.year2024
 
 class Day17(registers: List<String>, program: String) {
 
-    data class State(val a: Int, val b: Int, val c: Int, val ip: Int = 0, val output: List<String> = mutableListOf())
+    data class State(val a: Long, val b: Long, val c: Long, val ip: Int = 0, val output: List<String> = mutableListOf())
 
     private val program = program.substringAfter("Program: ").split(",").map(String::toInt)
     private val registers = registers.associate {
-        it.substringAfter("Register ").first().uppercase().first() to it.substringAfter(": ").toInt()
+        it.substringAfter("Register ").first().uppercase().first() to it.substringAfter(": ").toLong()
     }
 
 
     fun part1(): String {
         val initialState = State(registers['A']!!, registers['B']!!, registers['C']!!)
-//        println(initialState)
         val state = program.execute(initialState)
         val output = state.output.joinToString(",")
         if (DEBUG) println(output)
@@ -20,14 +19,24 @@ class Day17(registers: List<String>, program: String) {
     }
 
     fun part2(): String {
-        val output = program.joinToString(",")
-        var a = -1
-        do {
-//            if ((a % 100_000) == 0) println(a)
-            val state = program.execute(State(++a, registers['B']!!, registers['C']!!))
-//            println("$a -> ${state.output}")
-        } while (output != state.output.joinToString(","))
-        return a.toString()
+        val originalProgram = program.joinToString(",")
+        var candidates = listOf(0L)
+        while (candidates.isNotEmpty()) {
+            candidates = buildList {
+                for (base in candidates) {
+                    val start = base shl 3
+                    for (a in start..(start + 7)) {
+                        val state = program.execute(State(a, registers['B']!!, registers['C']!!))
+                        val output = state.output.joinToString(",")
+                        if (originalProgram == output)
+                            return a.toString()
+                        else if (originalProgram.endsWith(output))
+                            add(a)
+                    }
+                }
+            }
+        }
+        error("A not found!")
     }
 
 
@@ -55,9 +64,9 @@ class Day17(registers: List<String>, program: String) {
     sealed class Instruction(private val state: State) {
         abstract operator fun invoke(operand: Int): State
 
-        fun Int.combo(): Int =
+        fun Int.combo(): Long =
             when (this) {
-                in 0..3 -> this
+                in 0..3 -> this.toLong()
                 4 -> state.a
                 5 -> state.b
                 6 -> state.c
@@ -72,7 +81,7 @@ class Day17(registers: List<String>, program: String) {
     class Adv(private val state: State) : Instruction(state) {
         override fun invoke(operand: Int): State {
             val combo = operand.combo()
-            val value = (state.a shr combo)
+            val value = (state.a shr combo.toInt())
             debug("Adv($operand) -> A = ${state.a} >> $combo = $value")
             return State(value, state.b, state.c, state.ip + 2, state.output)
         }
@@ -80,7 +89,7 @@ class Day17(registers: List<String>, program: String) {
 
     class Blx(private val state: State) : Instruction(state) {
         override fun invoke(operand: Int): State {
-            val value = (state.b xor operand)
+            val value = (state.b xor operand.toLong())
             debug("Blx($operand) -> B = ${state.b} xor $operand = $value")
             return State(state.a, value, state.c, state.ip + 2, state.output)
         }
@@ -89,7 +98,7 @@ class Day17(registers: List<String>, program: String) {
     class Bst(private val state: State) : Instruction(state) {
         override fun invoke(operand: Int): State {
             val combo = operand.combo()
-            val value = (combo % 8)
+            val value = (combo and 0b111)
             debug("Bst($operand) -> B = $combo mod 8 = $value")
             return State(state.a, value, state.c, state.ip + 2, state.output)
         }
@@ -97,15 +106,13 @@ class Day17(registers: List<String>, program: String) {
 
     class Jnz(private val state: State) : Instruction(state) {
         override fun invoke(operand: Int): State {
-            return if (state.a == 0) {
+            return if (state.a == 0L) {
                 debug("Jnz($operand) -> A == 0 -> IP = IP + 2")
                 val state = State(0, state.b, state.c, state.ip + 2, state.output)
-//                println(state)
                 state
             } else {
                 debug("Jnz($operand) -> A == ${state.a} -> IP = $operand")
                 val state = State(state.a, state.b, state.c, operand, state.output)
-//                println(state)
                 state
             }
         }
@@ -131,7 +138,7 @@ class Day17(registers: List<String>, program: String) {
     class Bdv(private val state: State) : Instruction(state) {
         override fun invoke(operand: Int): State {
             val combo = operand.combo()
-            val value = (state.a shr combo)
+            val value = (state.a shr combo.toInt())
             debug("Bdv($operand) -> B = ${state.a} >> $combo = $value")
             return State(state.a, value, state.c, state.ip + 2, state.output)
         }
@@ -140,7 +147,7 @@ class Day17(registers: List<String>, program: String) {
     class Cdv(private val state: State) : Instruction(state) {
         override fun invoke(operand: Int): State {
             val combo = operand.combo()
-            val value = (state.a shr combo)
+            val value = (state.a shr combo.toInt())
             debug("Cdv($operand) -> C = ${state.a} >> $combo = $value")
             return State(state.a, state.b, value, state.ip + 2, state.output)
         }
