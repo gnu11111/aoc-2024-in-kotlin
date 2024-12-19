09f46a74e2018c34e2d6f7c647bad3b93a5f107d
gnu11111 <gnu@gnu.at>
2024-12-19 13:48:10 +0100

    polishing

--- src/main/kotlin/at/gnu/adventofcode/year2024/Day17.kt
+++ src/main/kotlin/at/gnu/adventofcode/year2024/Day17.kt
@@ -41,117 +41,99 @@ class Day17(registers: List<String>, program: String) {
 
 
     private fun List<Int>.execute(state: State): State {
-        var newState = state
-        while (newState.ip < size) {
-            val instruction = this[newState.ip]
-            val operand = this[newState.ip + 1]
-            newState = when (instruction) {
-                0 -> Adv(newState)(operand)
-                1 -> Blx(newState)(operand)
-                2 -> Bst(newState)(operand)
-                3 -> Jnz(newState)(operand)
-                4 -> Bxc(newState)(operand)
-                5 -> Out(newState)(operand)
-                6 -> Bdv(newState)(operand)
-                7 -> Cdv(newState)(operand)
+        var currentState = state
+        while (currentState.ip < size) {
+            val instruction = this[currentState.ip]
+            val operand = this[currentState.ip + 1]
+            currentState = when (instruction) {
+                0 -> Adv(operand)
+                1 -> Blx(operand)
+                2 -> Bst(operand)
+                3 -> Jnz(operand)
+                4 -> Bxc(operand)
+                5 -> Out(operand)
+                6 -> Bdv(operand)
+                7 -> Cdv(operand)
                 else -> error("no such operation: $instruction")
-            }
-            if (DEBUG) println(newState)
+            }.execute(currentState)
+            if (DEBUG) println(currentState)
         }
-        return newState
+        return currentState
     }
 
-    sealed class Instruction(private val state: State) {
-        abstract operator fun invoke(operand: Int): State
+    sealed class Instruction(val execute: (State) -> State) {
+        companion object {
+            fun Int.combo(state: State): Long =
+                when (this) {
+                    in 0..3 -> this.toLong()
+                    4 -> state.a
+                    5 -> state.b
+                    6 -> state.c
+                    else -> error("reserved operand: $this")
+                }
 
-        fun Int.combo(): Long =
-            when (this) {
-                in 0..3 -> this.toLong()
-                4 -> state.a
-                5 -> state.b
-                6 -> state.c
-                else -> error("reserved operand: $this")
-            }
-
-        fun debug(message: String) {
-            if (DEBUG) println(message)
-        }
-    }
-
-    class Adv(private val state: State) : Instruction(state) {
-        override fun invoke(operand: Int): State {
-            val combo = operand.combo()
-            val value = (state.a shr combo.toInt())
-            debug("Adv($operand) -> A = ${state.a} >> $combo = $value")
-            return State(value, state.b, state.c, state.ip + 2, state.output)
-        }
-    }
-
-    class Blx(private val state: State) : Instruction(state) {
-        override fun invoke(operand: Int): State {
-            val value = (state.b xor operand.toLong())
-            debug("Blx($operand) -> B = ${state.b} xor $operand = $value")
-            return State(state.a, value, state.c, state.ip + 2, state.output)
-        }
-    }
-
-    class Bst(private val state: State) : Instruction(state) {
-        override fun invoke(operand: Int): State {
-            val combo = operand.combo()
-            val value = (combo and 0b111)
-            debug("Bst($operand) -> B = $combo mod 8 = $value")
-            return State(state.a, value, state.c, state.ip + 2, state.output)
-        }
-    }
-
-    class Jnz(private val state: State) : Instruction(state) {
-        override fun invoke(operand: Int): State {
-            return if (state.a == 0L) {
-                debug("Jnz($operand) -> A == 0 -> IP = IP + 2")
-                val state = State(0, state.b, state.c, state.ip + 2, state.output)
-                state
-            } else {
-                debug("Jnz($operand) -> A == ${state.a} -> IP = $operand")
-                val state = State(state.a, state.b, state.c, operand, state.output)
-                state
+            fun debug(message: String) {
+                if (DEBUG) println(message)
             }
         }
     }
 
-    class Bxc(private val state: State) : Instruction(state) {
-        override fun invoke(operand: Int): State {
-            val value = state.b xor state.c
-            debug("Bxc($operand) -> B = ${state.b} xor ${state.c} = $value")
-            return State(state.a, value, state.c, state.ip + 2, state.output)
-        }
-    }
+    class Adv(private val operand: Int) : Instruction({ state ->
+        val combo = operand.combo(state)
+        val value = (state.a shr combo.toInt())
+        debug("Adv($operand) -> A = ${state.a} >> $combo = $value")
+        State(value, state.b, state.c, state.ip + 2, state.output)
+    })
 
-    class Out(private val state: State) : Instruction(state) {
-        override fun invoke(operand: Int): State {
-            val combo = operand.combo()
-            val value = combo % 8
-            debug("Out($operand) -> $combo mod 8 = $value")
-            return State(state.a, state.b, state.c, state.ip + 2, state.output + value.toString())
-        }
-    }
+    class Blx(private val operand: Int) : Instruction({ state ->
+        val value = (state.b xor operand.toLong())
+        debug("Blx($operand) -> B = ${state.b} xor $operand = $value")
+        State(state.a, value, state.c, state.ip + 2, state.output)
+    })
 
-    class Bdv(private val state: State) : Instruction(state) {
-        override fun invoke(operand: Int): State {
-            val combo = operand.combo()
-            val value = (state.a shr combo.toInt())
-            debug("Bdv($operand) -> B = ${state.a} >> $combo = $value")
-            return State(state.a, value, state.c, state.ip + 2, state.output)
-        }
-    }
+    class Bst(private val operand: Int) : Instruction({ state ->
+        val combo = operand.combo(state)
+        val value = (combo and 0b111)
+        debug("Bst($operand) -> B = $combo mod 8 = $value")
+        State(state.a, value, state.c, state.ip + 2, state.output)
+    })
 
-    class Cdv(private val state: State) : Instruction(state) {
-        override fun invoke(operand: Int): State {
-            val combo = operand.combo()
-            val value = (state.a shr combo.toInt())
-            debug("Cdv($operand) -> C = ${state.a} >> $combo = $value")
-            return State(state.a, state.b, value, state.ip + 2, state.output)
+    class Jnz(private val operand: Int) : Instruction({ state ->
+        if (state.a == 0L) {
+            debug("Jnz($operand) -> A == 0 -> IP = IP + 2")
+            State(0, state.b, state.c, state.ip + 2, state.output)
+        } else {
+            debug("Jnz($operand) -> A == ${state.a} -> IP = $operand")
+            State(state.a, state.b, state.c, operand, state.output)
         }
-    }
+    })
+
+    class Bxc(private val operand: Int) : Instruction({ state ->
+        val value = state.b xor state.c
+        debug("Bxc($operand) -> B = ${state.b} xor ${state.c} = $value")
+        State(state.a, value, state.c, state.ip + 2, state.output)
+    })
+
+    class Out(private val operand: Int) : Instruction({ state ->
+        val combo = operand.combo(state)
+        val value = combo % 8
+        debug("Out($operand) -> $combo mod 8 = $value")
+        State(state.a, state.b, state.c, state.ip + 2, state.output + value.toString())
+    })
+
+    class Bdv(private val operand: Int) : Instruction({ state ->
+        val combo = operand.combo(state)
+        val value = (state.a shr combo.toInt())
+        debug("Bdv($operand) -> B = ${state.a} >> $combo = $value")
+        State(state.a, value, state.c, state.ip + 2, state.output)
+    })
+
+    class Cdv(private val operand: Int) : Instruction({ state ->
+        val combo = operand.combo(state)
+        val value = (state.a shr combo.toInt())
+        debug("Cdv($operand) -> C = ${state.a} >> $combo = $value")
+        State(state.a, state.b, value, state.ip + 2, state.output)
+    })
 
     companion object {
         const val RESOURCE = "/adventofcode/year2024/Day17.txt"
